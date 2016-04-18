16befb895dfed93d8cf56880ef4b1c117006cbec
TBSHRichard <rvoelker@blacksheepherd.net>
2016-04-18 14:56:27 -0400

    Added functionality to levers and their associated connected objects.

---
+++ UnityProject/Caged/Assets/Scripts/LeverObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverObject : MonoBehaviour {
+    public Animator objectAnimator;
+    public bool isOpen;
+
+    void Start()
+    {
+        objectAnimator.SetBool("IsOpen", isOpen);
+    }
+
+    /*
+        Toggle the open state of the object.
+    */
+    public void Toggle()
+    {
+        isOpen = !isOpen;
+        objectAnimator.SetBool("IsOpen", isOpen);
+    }
+}
---
+++ UnityProject/Caged/Assets/Scripts/LeverRightClickAction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class LeverRightClickAction : RightClickAction {
+    public LeverObject[] connectedObjects;
+    public Animator leverAnimator;
+
+    private bool _isOn;
+
+    public override void ActionCommand(Player player)
+    {
+        _isOn = !_isOn;
+        leverAnimator.SetBool("IsOn", _isOn);
+
+        foreach (LeverObject leverObject in connectedObjects)
+        {
+            leverObject.Toggle();
+        }
+    }
+
+    public override bool CanAct(Player player)
+    {
+        return player.form == AnimalForm.Snake;
+    }
+}
